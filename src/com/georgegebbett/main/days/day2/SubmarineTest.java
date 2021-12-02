ffb3bd10b420f7a9255cf7de4481045e19fbc700
George Gebbett <george.gebbett1@gmail.com>
2021-12-02 10:07:50 +0000

    Better tests

--- src/com/georgegebbett/main/days/day2/SubmarineTest.java
+++ src/com/georgegebbett/main/days/day2/SubmarineTest.java
@@ -1,5 +1,6 @@
 package com.georgegebbett.main.days.day2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
@@ -9,19 +10,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubmarineTest {
 
     @Test
-    void part1() throws SubmarineException {
+    @DisplayName("1. Part 1 returns 150 with test input")
+    void Part1Returns150WithTestInput() throws SubmarineException {
         Submarine testSub = new Submarine("/Users/george/IdeaProjects/adventofcode2021/src/com/georgegebbett/main/days/day2/testInput.txt");
         assertEquals(testSub.part1(), 150);
     }
 
     @Test
-    void part2() throws SubmarineException {
+    @DisplayName("2. Part 2 returns 900 with test input")
+    void Part2Returns900WithTestInput() throws SubmarineException {
         Submarine testSub = new Submarine("/Users/george/IdeaProjects/adventofcode2021/src/com/georgegebbett/main/days/day2/testInput.txt");
         assertEquals(testSub.part2(), 900);
     }
 
     @Test
-    void invalidFile() {
+    @DisplayName("3. Creating a Submarine with an invalid file results in a SubmarineException")
+    void CreatingSubmarineWithInvalidFileResultsInSubmarineException() {
         assertThrows(SubmarineException.class, () -> new Submarine("invalid path"));
+    }
+
+    @Test
+    @DisplayName("4. Running the parts on a file with an invalid direction results in a SubmarineException")
+    void invalidDirectionTest() throws SubmarineException {
+        Submarine badSub = new Submarine("/Users/george/IdeaProjects/adventofcode2021/src/com/georgegebbett/main/days/day2/badInput.txt");
+        assertThrows(SubmarineException.class, badSub::part1);
+        assertThrows(SubmarineException.class, badSub::part2);
     }
 }
